f5a3772c16d85655b3323d6db7990bf799acf72c
Pranto-Nondie <pranto.cs.25@gmail.com>
2023-05-21 21:37:35 +0600

    updated

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -54,77 +54,79 @@ const Banner = () => {
     }, []);
 
     return (
-        <div className="flex justify-center items-center">
-            <div className="carousel w-full h-[600px]" ref={carouselRef}>
-                <div id="slide1" className="carousel-item relative w-full active">
-                    <img src="https://i.ibb.co/zRRvMJJ/st.jpg" className="w-full" />
-                    <div className="absolute flex justify-center items-center top-0 left-0 w-full h-full bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
-                        <div className="space-y-8 text-center">
-                            <h1 className="text-6xl text-white font-semibold">Explore the Wonderful World of Kids' Toys</h1>
-                            <p className="text-2xl text-white font-semibold">Discover a wide range of toys that ignite imagination and bring joy to children</p>
-                            <div className="flex justify-center">
-                                {buttons}
+        <>
+            <div className="flex justify-center items-center">
+                <div className="carousel w-full h-[600px]" ref={carouselRef}>
+                    <div id="slide1" className="carousel-item relative w-full active">
+                        <img src="https://i.ibb.co/zRRvMJJ/st.jpg" className="w-full" />
+                        <div className="absolute flex justify-center items-center top-0 left-0 w-full h-full bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+                            <div className="space-y-8 text-center">
+                                <h1 className="text-6xl text-white font-semibold">Explore the Wonderful World of Kids' Toys</h1>
+                                <p className="text-2xl text-white font-semibold">Discover a wide range of toys that ignite imagination and bring joy to children</p>
+                                <div className="flex justify-center">
+                                    {buttons}
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide4" className="btn btn-circle mr-4">❮</a>
-                        <a href="#slide2" className="btn btn-circle mr-4">❯</a>
-                    </div>
-                </div>
-                <div id="slide2" className="carousel-item relative w-full">
-                    <img src="https://i.ibb.co/MgdgZYH/bt.jpg" className="w-full" />
-                    <div className="absolute flex justify-center items-center top-0 left-0 w-full h-full bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
-                        <div className="space-y-8 text-center">
-                            <h1 className="text-6xl text-white font-semibold">
-                                Explore the Wonderful World of Kids' Toys</h1>
-                            <p className="text-2xl text-white font-semibold">Discover a wide range of toys that ignite imagination and bring joy to children</p>
-                            <div className="flex justify-center">
-                                {buttons}
-                            </div>
+                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                            <a href="#slide4" className="btn btn-circle mr-4">❮</a>
+                            <a href="#slide2" className="btn btn-circle mr-4">❯</a>
                         </div>
                     </div>
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide1" className="btn btn-circle mr-4">❮</a>
-                        <a href="#slide3" className="btn btn-circle mr-4">❯</a>
-                    </div>
-                </div>
-                <div id="slide3" className="carousel-item relative w-full">
-                    <img src="https://i.ibb.co/bdmP9dm/ut.jpg" className="w-full" />
-                    <div className="absolute flex justify-center items-center top-0 left-0 w-full h-full bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
-                        <div className="space-y-8 text-center">
-                            <h1 className="text-6xl text-white font-semibold">
-                                Explore the Wonderful World of Kids' Toys</h1>
-                            <p className="text-2xl text-white font-semibold">Discover a wide range of toys that ignite imagination and bring joy to children</p>
-                            <div className="flex justify-center">
-                                {buttons}
+                    <div id="slide2" className="carousel-item relative w-full">
+                        <img src="https://i.ibb.co/MgdgZYH/bt.jpg" className="w-full" />
+                        <div className="absolute flex justify-center items-center top-0 left-0 w-full h-full bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+                            <div className="space-y-8 text-center">
+                                <h1 className="text-6xl text-white font-semibold">
+                                    Explore the Wonderful World of Kids' Toys</h1>
+                                <p className="text-2xl text-white font-semibold">Discover a wide range of toys that ignite imagination and bring joy to children</p>
+                                <div className="flex justify-center">
+                                    {buttons}
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide2" className="btn btn-circle mr-4">❮</a>
-                        <a href="#slide4" className="btn btn-circle mr-4">❯</a>
-                    </div>
-                </div>
-                <div id="slide4" className="carousel-item relative w-full">
-                    <img src="https://i.ibb.co/b3kSZdH/dt.jpg" className="w-full" />
-                    <div className="absolute flex justify-center items-center top-0 left-0 w-full h-full bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
-                        <div className="space-y-8 text-center">
-                            <h1 className="text-6xl text-white font-semibold">
-                                Explore the Wonderful World of Kids' Toys</h1>
-                            <p className="text-2xl text-white font-semibold">Discover a wide range of toys that ignite imagination and bring joy to children</p>
-                            <div className="flex justify-center">
-                                {buttons}
-                            </div>
+                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                            <a href="#slide1" className="btn btn-circle mr-4">❮</a>
+                            <a href="#slide3" className="btn btn-circle mr-4">❯</a>
                         </div>
                     </div>
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide3" className="btn btn-circle mr-4">❮</a>
-                        <a href="#slide1" className="btn btn-circle mr-4">❯</a>
+                    <div id="slide3" className="carousel-item relative w-full">
+                        <img src="https://i.ibb.co/bdmP9dm/ut.jpg" className="w-full" />
+                        <div className="absolute flex justify-center items-center top-0 left-0 w-full h-full bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+                            <div className="space-y-8 text-center">
+                                <h1 className="text-6xl text-white font-semibold">
+                                    Explore the Wonderful World of Kids' Toys</h1>
+                                <p className="text-2xl text-white font-semibold">Discover a wide range of toys that ignite imagination and bring joy to children</p>
+                                <div className="flex justify-center">
+                                    {buttons}
+                                </div>
+                            </div>
+                        </div>
+                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                            <a href="#slide2" className="btn btn-circle mr-4">❮</a>
+                            <a href="#slide4" className="btn btn-circle mr-4">❯</a>
+                        </div>
+                    </div>
+                    <div id="slide4" className="carousel-item relative w-full">
+                        <img src="https://i.ibb.co/b3kSZdH/dt.jpg" className="w-full" />
+                        <div className="absolute flex justify-center items-center top-0 left-0 w-full h-full bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+                            <div className="space-y-8 text-center">
+                                <h1 className="text-6xl text-white font-semibold">
+                                    Explore the Wonderful World of Kids' Toys</h1>
+                                <p className="text-2xl text-white font-semibold">Discover a wide range of toys that ignite imagination and bring joy to children</p>
+                                <div className="flex justify-center">
+                                    {buttons}
+                                </div>
+                            </div>
+                        </div>
+                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                            <a href="#slide3" className="btn btn-circle mr-4">❮</a>
+                            <a href="#slide1" className="btn btn-circle mr-4">❯</a>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Banner from '../Banner/Banner';
 import Gallery from '../Gallery/Gallery';
 import ToyTabCategory from '../ToyTabCategory/ToyTabCategory';
 import LatestToy from '../LatestToy/LatestToy';
 import useTitle from '../../../hooks/useTitle';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 
 
 const Home = () => {
+    useEffect(() => {
+        AOS.init();
+    }, []);
+
+    useEffect(() => {
+        AOS.refresh();
+    });
     useTitle(`Home`)
     return (
-        <div>
+        <div data-aos="fade-up" >
             <Banner></Banner>
             <Gallery></Gallery>
             <ToyTabCategory></ToyTabCategory>
